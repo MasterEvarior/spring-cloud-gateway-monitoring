6b66347634cb29d849ab0252eb83e7ed933766cd
MasterEvarior <contaxt@giannin.dev>
2022-10-21 18:36:05 +0200

    Initial commit

---
+++ src/test/java/dev/giannin/springcloudgatewaymonitoring/SpringCloudGatewayMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.giannin.springcloudgatewaymonitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudGatewayMonitoringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
